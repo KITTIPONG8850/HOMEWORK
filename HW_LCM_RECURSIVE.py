2fa4e08e1f906055f16d455e68a0a6d791a5e083
KITTIPONG8850 <kittipongsuwannarak@gmail.com>
2023-02-16 13:41:21 +0700

    add file to project

---
+++ HW_LCM_RECURSIVE.py
@@ -0,0 +1,20 @@
+def lcm_recursive(*args):   
+    def lcm(x, y):
+        lcm=0
+        if x > y:
+           higher = x
+        else:
+           higher = y    
+        while True:
+            if higher % x == 0 and higher % y == 0:
+                lcm = higher
+                break
+            else:
+                higher = higher +1 
+        return lcm
+    if len(args)==2:
+        result =lcm(args[0],args[1])
+        return result
+    return lcm_recursive(args[0],lcm_recursive(*args[1:]))
+
+print(lcm_recursive(2,5,8,51))
